63016a2190fad9f0efc5e9a6b3bdff06d764016c
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2022-03-25 07:49:07 -0500

    did major formatting in defs.py, as well as some formatting in dsame.py
    additional formatting was done in the ./scripts/ files

--- scripts/source-rtl-fm-multimon-ng.sh
+++ scripts/source-rtl-fm-multimon-ng.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-echo INPUT: rtl_fm Device 1>&2
-PPM=0
-FREQ=162.500M
-GAIN=42
-until rtl_fm -f ${FREQ} -M fm -s 22050 -E dc -p ${PPM} -g ${GAIN}  -|  multimon-ng -t raw -a EAS /dev/stdin; do
-    echo Restarting... >&2
-    sleep 2
-done
 
+echo INPUT: rtl_fm Device 1>&2
+
+PPM=0
+#FREQ=162450000
+#FREQ=162475000
+#FREQ=162500000
+#FREQ=162525000
+FREQ=162550000
+GAIN=42
+
+until rtl_fm -f ${FREQ} -M fm -s 22050 -E dc -p ${PPM} -g ${GAIN} - | multimon-ng -t raw -a EAS /dev/stdin; do
+	echo Restarting... >&2
+	sleep 2
+done
